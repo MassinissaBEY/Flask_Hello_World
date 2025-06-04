bcbc0406abfd33c1fd6de495b075cf2d616c3658
MassinissaBEY <104314110+MassinissaBEY@users.noreply.github.com>
2025-06-04 16:53:24 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -56,7 +56,7 @@ def valeur_maximale(nombres):
 def cv():
     return render_template("cv.html")
 
-pythonif __name__ == "__main__":
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
 # if __name__ == "__main__":
 #   app.run(debug=True)   #commit222
